d2dda104c495a5eea0dcf5a03b874b1dcd67e4c7
leonwoods53 <61557365+leonwoods53@users.noreply.github.com>
2023-03-26 19:09:52 +0100

    Add files via upload

--- LoginPage.java
+++ LoginPage.java
@@ -1,85 +1,121 @@
-package projectyear2oosd;
+package project;
 
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
 
-public class LoginPage implements ActionListener{
+public class LoginPage {
 
-   private static JLabel userLabel;
-   private static JTextField userText;
-   private static JLabel passwordLabel;
-   private static JPasswordField passwordText;
-   private static JButton loginButton;
-   private static JButton signupButton;
-   private static JLabel success;
+   	   private static JLabel userLabel;
+	   private static JTextField userText;
+	   private static JLabel passwordLabel;
+	   private static JPasswordField passwordText;
+	   private static JButton loginButton;
+	
 
-   public static void main(String []args) {
+	   public static void main(String []args) {
+		   
+		   SwingUtilities.invokeLater(() -> {
+	            JFrame frame = createLoginFrame();
+	            frame.setVisible(true);
+	        });
+	    }
 
-      JPanel panel = new JPanel();
-      JFrame frame = new JFrame();
-      frame.setSize(350,200);
-      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      frame.setVisible(true);
-      frame.add(panel);
-      
-      panel.setLayout(null);
+	    private static JFrame createLoginFrame() {
+	        JFrame frame = new JFrame("Login");
+	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	        frame.setSize(300, 200);
 
-      userLabel = new JLabel("Username");
-      userLabel.setBounds(10, 20, 80, 25);
-      panel.add(userLabel);
+	      JPanel panel = new JPanel();
+	      frame.setSize(350,200);
+	      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	      frame.setVisible(true);
+	      frame.add(panel);
+	      
+	      panel.setLayout(null);
 
-      userText = new JTextField();
-      userText.setBounds(100, 20, 165, 25);
-      panel.add(userText);
+	      userLabel = new JLabel("Username");
+	      userLabel.setBounds(10, 20, 80, 25);
+	      panel.add(userLabel);
 
-      passwordLabel = new JLabel("Password");
-      passwordLabel.setBounds(10, 50, 80, 25);
-      panel.add(passwordLabel);
+	      userText = new JTextField();
+	      userText.setBounds(100, 20, 165, 25);
+	      panel.add(userText);
 
-      passwordText = new JPasswordField();
-      passwordText.setBounds(100,50,165,25);
-      panel.add(passwordText);
+	      passwordLabel = new JLabel("Password");
+	      passwordLabel.setBounds(10, 50, 80, 25);
+	      panel.add(passwordLabel);
 
-      loginButton = new JButton("Login");
-      loginButton.setBounds(10,80,80,25);
-      loginButton.addActionListener((ActionListener) new SignUpPage());
-      panel.add(loginButton);
+	      passwordText = new JPasswordField();
+	      passwordText.setBounds(100,50,165,25);
+	      panel.add(passwordText);
 
-      signupButton = new JButton("Sign Up");
-      signupButton.setBounds(100,80,80,25);
-      panel.add(signupButton);
+	      loginButton = new JButton("Login");
+	      loginButton.setBounds(10,80,80,25);
+	      panel.add(loginButton);
 
-      success = new JLabel("");
-      success.setBounds(10,110,300,25);
-      panel.add(success);
-      
+	      loginButton.addActionListener(new ActionListener() {
+	            @Override
+	            public void actionPerformed(ActionEvent e) {
+	                String username = userText.getText();
+	                String password = new String(passwordText.getPassword());
 
-      frame.setVisible(true);
+	                try {
+	                    if (authenticateUser(username, password)) {
+	                        JOptionPane.showMessageDialog(frame, "Login successful!");
+	                        frame.dispose();
+	                        SwingUtilities.invokeLater(() -> {
+	                            JFrame mainMenu = new MainMenu();
+	                            mainMenu.setVisible(true);
+	                            
+	                           
+	                        });
+	                        // Perform any post-login actions here
+	                    } else {
+	                        JOptionPane.showMessageDialog(frame, "Invalid username or password.");
+	                    }
+	                } catch (SQLException ex) {
+	                    JOptionPane.showMessageDialog(frame, "Error: " + ex.getMessage());
+	                }
+	            }	
+				
+	        });
+	      
+	      frame.add(panel);
+	      frame.setVisible(true);
+	      return frame;
+  	   	 
+	   }
+	   
 
-      
-   
- 
-   }
-   
+	    private static boolean authenticateUser(String username, String password) throws SQLException {
+	        String url = "jdbc:mysql://localhost/oosdProject";
+	        String user = "root";
+	        String pass = "";
 
+	        try (Connection connection = DriverManager.getConnection(url, user, pass)) {
+	            String sql = "SELECT * FROM User WHERE username = ? AND password = ?";
+	            try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+	                preparedStatement.setString(1, username);
+	                preparedStatement.setString(2, password);
 
-   @Override
-   public void actionPerformed(java.awt.event.ActionEvent arg0) {
-      // TODO Auto-generated method stub
-      String user = userText.getText();
-      String password = passwordText.getText();
-      System.out.println(user + ", " + password);
-
-      if(user.equals("Leon") && password.equals("hello")) {
-         success.setText("Login Successful");
-      }
-      
-   }
-
+	                try (ResultSet resultSet = preparedStatement.executeQuery()) {
+	                    return resultSet.next();
+	                }
+	            }
+	        }
+	    }
 }
