372aa8ea9339de371fee6bf4ebf84b0a2549a9b5
leonwoods53 <61557365+leonwoods53@users.noreply.github.com>
2023-03-26 19:10:05 +0100

    Add files via upload

---
+++ MainMenu.java
@@ -0,0 +1,15 @@
+package project;
+
+import javax.swing.JFrame;
+
+public class MainMenu extends JFrame {
+	
+	public MainMenu() {
+        
+		setTitle("Main Menu");
+        setSize(700, 700);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+	
+}
+}
