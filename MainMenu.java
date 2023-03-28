917c7a6f1b43e693a05e987ebf0083946bcc601d
leonwoods53 <61557365+leonwoods53@users.noreply.github.com>
2023-03-28 18:00:14 +0100

    Add files via upload

--- MainMenu.java
+++ MainMenu.java
@@ -1,15 +1,44 @@
 package project;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class MainMenu extends JFrame {
+
 	
 	public MainMenu() {
         
-		setTitle("Main Menu");
-        setSize(700, 700);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JPanel panel = new JPanel();
+	    JFrame frame = new JFrame();
+	    frame.setSize(750,350);
+	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    frame.setVisible(true);
+	    frame.add(panel);
+	    panel.setLayout(null);
+	    
+        JLabel Header = new JLabel("CW Sports");
+        Header.setBounds(340,20,150,25);
+        panel.add(Header);
         
+        JLabel Info = new JLabel("Select an Option Below");
+        Info.setBounds(300,100,150,25);
+        panel.add(Info);
+        
+       JButton Customer = new JButton("Customer");
+       Customer.setBounds(100,200,150,25);
+       panel.add(Customer);
+       
+       JButton Products = new JButton("Products");
+       Products.setBounds(300,200,150,25);
+       panel.add(Products);
+       
+       JButton Invoices = new JButton("Invoices");
+       Invoices.setBounds(517,200,150,25);
+       panel.add(Invoices);
+       
+       
 	
 }
 }
